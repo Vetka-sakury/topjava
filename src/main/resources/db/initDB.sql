00b349ee386bf71ac193415851ebe5df4244b9ce
Vetka-sakury <alfiya.miftahova@yandex.ru>
2021-02-24 22:39:49 +0300

    HW03

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -23,3 +24,16 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    user_id          INTEGER      NOT NULL,
+    meal_id          INTEGER      DEFAULT nextval('global_seq'),
+    date_time        TIMESTAMP    NOT NULL,
+    description      VARCHAR      NOT NULL,
+    calories         INTEGER      NOT NULL,
+    CONSTRAINT meals_datetime_idx UNIQUE (user_id, date_time),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE INDEX meals_by_users_idx ON meals (user_id);
+
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,13 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, TO_TIMESTAMP ('2020-01-30 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Завтрак', 500),
+       (100000, TO_TIMESTAMP ('2020-01-30 13:00:00', 'YYYY-MM-DD HH24:MI:SS') , 'Обед', 1000),
+       (100000, TO_TIMESTAMP ('2020-01-30 20:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Ужин', 500),
+  (100001, TO_TIMESTAMP ('2020-01-31 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Еда на граничное значение', 100),
+  (100001, TO_TIMESTAMP ('2020-01-31 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Завтрак', 1000),
+  (100001, TO_TIMESTAMP ('2020-01-31 13:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Обед', 500),
+  (100001, TO_TIMESTAMP ('2020-01-31 20:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Ужин', 410);
+
